3db38a34a3ec74ee9d4284e85006f31b99c75ab1
Your Name <vishwadipsingale99@gmail.com>
2022-03-31 12:24:05 +0530

    first commit

---
+++ src/main/java/com/zensar/webapp/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
