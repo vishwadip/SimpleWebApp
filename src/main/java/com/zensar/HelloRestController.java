6e697c772b13ad14ef9625d0049aac84ac0bc703
vishwadip <39756394+vishwadip@users.noreply.github.com>
2022-03-31 16:37:23 +0530

    Update HelloRestController.java

--- src/main/java/com/zensar/HelloRestController.java
+++ src/main/java/com/zensar/HelloRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloRestController{
-  @GetMapping("/hello/")
+  @GetMapping("/hello")
   public String getHelloMessage()
   {
     return "HelloWold";
